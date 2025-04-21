0dcd5e532aab0dc192ade0d04437c9a3052f04db
Angel Mosquea <mosqueaangel07@gmail.com>
2025-04-20 21:53:29 -0400

    Eliminación de porción comentada

--- ocr_extractor.py
+++ ocr_extractor.py
@@ -60,11 +60,6 @@ def process_invoice(image_path):
     itbis_facturado = clean_number(extract_value(itbis_pattern, extracted_text))
     retencion = clean_number(extract_value(retencion_pattern, extracted_text))
 
-    # if (ncf.startswith("B03") or ncf.startswith("B04")) and not ncf_modificado:
-    #     ncf_modificado = correct_ncf_format(
-    #         extract_value(r"B[Oo0I1]{2}\d+", extracted_text, "")
-    #     )
-
     if ncf.startswith("B01"):
         ncf_modificado = "No aplica para NCF tipo B01"
     elif (ncf.startswith("B03") or ncf.startswith("B04")) and not ncf_modificado:
